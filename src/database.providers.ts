5f6d361e3fa04bbe0bdcceb7a0da281cbfc26bc4
lienlq <lien.lequang90@gmail.com>
2024-05-21 20:51:31 +0700

    a

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -7,11 +7,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username:  'root',
-        password:  'suIp@y2024',
-        database: 'suidona',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+        username: process.env.DATABASE_USER || 'root',
+        password: process.env.DATABASE_PASSWORD || 'suIp@y2024',
+        database: process.env.DATABASE_NAME || 'suidona',
         entities: [join(__dirname, '**', '*.entity.{ts,js}')],
         synchronize: true,
       });
